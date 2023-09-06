bde036bb3d486447ac67765aad4c292df07cd216
Cmar <felix.mar1995@hotmail.com>
2023-09-06 09:46:08 -0600

    Agregando section de about us y what i do

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ const Header = () => {
   return (
     <header className='py-8' >
       <div className="container mx-auto">
-        <div className='flex justify-end items-center'>
+        <div className='flex justify-between items-center'>
           {/* logo */}
-          {/* <a href="#">
+          <a href="#">
             <img src={Logo} alt="" />
-          </a> */}
+          </a>
           <button className='btn btn-sm'>Work whit me </button>
         </div>
       </div>
